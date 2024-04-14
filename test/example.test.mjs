ebed720570e826d9c7975ebb84947bd4a5f0c140
David Andrea <davejandrea@gmail.com>
2024-04-14 14:40:23 -0300

    refactor

--- test/example.test.mjs
+++ test/example.test.mjs
@@ -20,54 +20,57 @@ x = 2, y = 2, rule = B3/S23
 
 describe("Game of Life", () => {
 
-  test("should parse width of input from metadata", () => {
-    const gol = new GameOfLife();
-    gol.parseRLEString(glider);
-    expect(gol.width).to.equal(3);
-  })
+  describe("reading and writing the rle patterns",() => {
+    test("should parse width of input from metadata", () => {
+      const gol = new GameOfLife();
+      gol.parseRLEString(glider);
+      expect(gol.width).to.equal(3);
+    })
 
-  test("should parse height of input from metadata", () => {
-    const gol = new GameOfLife();
-    gol.parseRLEString(glider);
-    expect(gol.height).to.equal(3);
-  })
+    test("should parse height of input from metadata", () => {
+      const gol = new GameOfLife();
+      gol.parseRLEString(glider);
+      expect(gol.height).to.equal(3);
+    })
 
-  test("should parse an RLE string input and return as matrix", () => {
-    const gol = new GameOfLife();
-    gol.parseRLEString(glider);
-    const expected = [
-      ["b", "o", "b"],
-      ["b", "b", "o"],
-      ["o", "o", "o"]
-    ];
-    expect(gol.startingShape).to.deep.equal(expected);
-  });
+    test("should parse an RLE string input and return as matrix", () => {
+      const gol = new GameOfLife();
+      gol.parseRLEString(glider);
+      const expected = [
+        ["b", "o", "b"],
+        ["b", "b", "o"],
+        ["o", "o", "o"]
+      ];
+      expect(gol.startingShape).to.deep.equal(expected);
+    });
 
-  test("should output the matrix as an RLE string", () => {
-    const game = new GameOfLife();
-    game.parseRLEString(glider);
-    const result = game.outputRLE();
-    expect(result).to.deep.equal("bob$2bo$3o!");
-  })
+    test("should output the matrix as an RLE string", () => {
+      const game = new GameOfLife();
+      game.parseRLEString(glider);
+      const result = game.outputRLE();
+      expect(result).to.deep.equal("bob$2bo$3o!");
+    })
 
-  test("should read string from RLE file", async () => {
-    const expected = `#N Glider
+    test("should read string from RLE file", async () => {
+      const expected = `#N Glider
 #O Richard K. Guy
 #C The smallest, most common, and first discovered spaceship. Diagonal, has period 4 and speed c/4.
 #C www.conwaylife.com/wiki/index.php?title=Glider
 x = 3, y = 3, rule = B3/S23
 bob$2bo$3o!`
 
-    const result = await GameOfLife.readFile("test/glider.rle");
-    expect(result).to.deep.equal(expected);
-  });
+      const result = await GameOfLife.readFile("test/glider.rle");
+      expect(result).to.deep.equal(expected);
+    });
 
-  test("should return pattern in rle format including metadata", () => {
-    const game = new GameOfLife();
-    game.parseRLEString(glider);
-    const result = game.outputFullRLE();
-    expect(result).to.deep.equal(glider);
-  });
+    test("should return pattern in rle format including metadata", () => {
+      const game = new GameOfLife();
+      game.parseRLEString(glider);
+      const result = game.outputFullRLE();
+      expect(result).to.deep.equal(glider);
+    });
+  })
+
 
   test("should return all neighbors of a given cell", () => {
     const shape = [
@@ -108,29 +111,6 @@ bob$2bo$3o!`
 
     expect(result).to.deep.equal(expected);
   });
-
-
-  // must test on a game board larger than the given block
-  test("should return next generation of game board from given board", () => {
-    const shape = [
-      ["b", "o", "b"],
-      ["b", "b", "o"],
-      ["o", "o", "o"]
-    ];
-
-    const HEIGHT = 5;
-    const WIDTH = 5
-
-    const game = new GameOfLife();
-    const board2 = new Board(5, 5);
-    board2.placeShape(shape, 1, 1);
-    board2.setGrid(game.evolve(board2, HEIGHT, WIDTH));
-    const result2 = board2.isolateShape(3, 3);
-    const expected = [ [ 'o', 'b', 'o' ], [ 'b', 'o', 'o' ], [ 'b', 'o', 'b' ] ];
-
-    expect(result2).to.deep.equal(expected);
-  });
-
 
   test("should generate game board full of 'dead' cells", () => {
     const expected = new Board(10, 12).grid;
@@ -188,4 +168,25 @@ bob$2bo$3o!`
 
     expect(result).to.deep.equal(expected);
   })
+
+  // must test on a game board larger than the given block
+  test("should return next generation of game board from given board", () => {
+    const shape = [
+      ["b", "o", "b"],
+      ["b", "b", "o"],
+      ["o", "o", "o"]
+    ];
+
+    const HEIGHT = 5;
+    const WIDTH = 5
+
+    const game = new GameOfLife();
+    const board2 = new Board(5, 5);
+    board2.placeShape(shape, 1, 1);
+    board2.setGrid(game.evolve(board2, HEIGHT, WIDTH));
+    const result2 = board2.isolateShape(3, 3);
+    const expected = [ [ 'o', 'b', 'o' ], [ 'b', 'o', 'o' ], [ 'b', 'o', 'b' ] ];
+
+    expect(result2).to.deep.equal(expected);
+  });
 });
