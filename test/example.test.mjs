476906b81f3586955d6a3e373d607b02b1fe6f5d
David Andrea <davejandrea@gmail.com>
2024-04-14 14:58:46 -0300

    test gen 2 and 3 for glider

--- test/example.test.mjs
+++ test/example.test.mjs
@@ -20,7 +20,7 @@ x = 2, y = 2, rule = B3/S23
 
 describe("Game of Life", () => {
 
-  describe("reading and writing the rle patterns",() => {
+  describe("reading and writing the rle patterns", () => {
     test("should parse width of input from metadata", () => {
       const gol = new GameOfLife();
       gol.parseRLEString(glider);
@@ -129,7 +129,7 @@ bob$2bo$3o!`
       const game = new GameOfLife()
       const board2 = new Board(4, 4);
 
-      const shape  = [
+      const shape = [
         ["b", "o", "b"],
         ["b", "b", "o"],
         ["o", "o", "o"]
@@ -170,27 +170,62 @@ bob$2bo$3o!`
     })
   })
 
-describe("core game logic", () => {
-  // must test on a game board larger than the given block
-  test("should return next generation of game board from given board", () => {
+  describe("core game logic", () => {
+
+    const game = new GameOfLife();
     const shape = [
       ["b", "o", "b"],
       ["b", "b", "o"],
       ["o", "o", "o"]
     ];
 
-    const HEIGHT = 5;
-    const WIDTH = 5
+    // must test on a game board larger than the given block
+    test("should return next generation of game board from given board", () => {
 
-    const game = new GameOfLife();
-    const board2 = new Board(5, 5);
-    board2.placeShape(shape, 1, 1);
-    board2.setGrid(game.evolve(board2, HEIGHT, WIDTH));
-    const result2 = board2.isolateShape(3, 3);
-    const expected = [ [ 'o', 'b', 'o' ], [ 'b', 'o', 'o' ], [ 'b', 'o', 'b' ] ];
 
-    expect(result2).to.deep.equal(expected);
-  });
+      const HEIGHT = 5;
+      const WIDTH = 5
+
+      const board = new Board(5, 5);
+      board.placeShape(shape, 1, 1);
+      board.setGrid(game.evolve(board, HEIGHT, WIDTH));
+      const result = board.isolateShape(3, 3);
+      const expected = [['o', 'b', 'o'], ['b', 'o', 'o'], ['b', 'o', 'b']];
+
+      expect(result).to.deep.equal(expected);
+    });
+
+    test("should return proper shape after 2 generations", () => {
+      const HEIGHT = 8;
+      const WIDTH = 8;
+      const board = new Board(HEIGHT, WIDTH);
+      board.placeShape(shape, 2, 2);
+      board.setGrid(game.evolve(board, HEIGHT, WIDTH));
+      board.setGrid(game.evolve(board, HEIGHT, WIDTH));
+      const result = board.isolateShape(3, 3);
+      const expected = [["b", "b", "o"], ["o", "b", "o"], ["b", "o", "o"]] // need shape
+
+      expect(result).to.deep.equal(expected);
+    })
+
+    test("should return proper shape after 3 generations", () => {
+      const HEIGHT = 8;
+      const WIDTH = 8;
+      const board = new Board(HEIGHT, WIDTH);
+      board.placeShape(shape, 2, 2);
+
+      board.setGrid(game.evolve(board, HEIGHT, WIDTH));
+      board.setGrid(game.evolve(board, HEIGHT, WIDTH));
+      board.setGrid(game.evolve(board, HEIGHT, WIDTH));
+      // board.setGrid(game.evolve(board, HEIGHT, WIDTH));
+
+      const result = board.isolateShape(3, 3);
+      const expected = [["o", "b", "b"], ["b", "o", "o"], ["o", "o", "b"]] // need shape
+
+      expect(result).to.deep.equal(expected);
+    })
+
+
+  })
+
 })
-
-});
