4c401eae40c17faa2626942d8f763f1874a1e2b8
David Andrea <davejandrea@gmail.com>
2024-04-23 09:33:35 -0300

    move examples and exerpts from snark-loop to own file

---
+++ test/snark-fragments.mjs
@@ -0,0 +1,95 @@
+export const snark0 = "x = 19, y = 13, rule = B3/S23\n" +
+  "8b2o$8bobo$10bo4b2o$6b4ob2o2bo2bo$6bo2bo3bobob2o$9bobobobo$10b2obobo$\n" +
+  "14bo2$2o$bo8bo$bobo5b2o$2b2o!\n"
+
+export const snark1 = "x = 19, y = 13, rule = B3/S23\n" +
+  "8b2o$8bobo$10bo4b2o$6b4ob2o2bo2bo$6bo2bo3bobob2o$9bobobobo$10b2obobo$\n" +
+  "14bo2$2o$bo8bo$bobo5b2o$2b2o!"
+
+export const snark2 = "x = 19, y = 13, rule = B3/S23\n" +
+  "8b2o$8bobo$10bo4b2o$6b4ob2o2bo2bo$6bo2bobobobob2o$9bobobobo$10b2obobo\n" +
+  "$14bo2$2o$bo7b2o$bobo5b2o$2b2o!\n"
+
+
+export const snarkArr = [
+  [
+    'b', 'b', 'b', 'b', 'b',
+    'b', 'b', 'b', 'o', 'o',
+    'b', 'b', 'b', 'b', 'b',
+    'b', 'b', 'b', 'b'
+  ],
+  [
+    'b', 'b', 'b', 'b', 'b',
+    'b', 'b', 'b', 'o', 'b',
+    'o', 'b', 'b', 'b', 'b',
+    'b', 'b', 'b', 'b'
+  ],
+  [
+    'b', 'b', 'b', 'b', 'b',
+    'b', 'b', 'b', 'b', 'b',
+    'o', 'b', 'b', 'b', 'b',
+    'o', 'o', 'b', 'b'
+  ],
+  [
+    'b', 'b', 'b', 'b', 'b',
+    'b', 'o', 'o', 'o', 'o',
+    'b', 'o', 'o', 'b', 'b',
+    'o', 'b', 'b', 'o'
+  ],
+  [
+    'b', 'b', 'b', 'b', 'b',
+    'b', 'o', 'b', 'b', 'o',
+    'b', 'b', 'b', 'o', 'b',
+    'o', 'b', 'o', 'o'
+  ],
+  [
+    'b', 'b', 'b', 'b', 'b',
+    'b', 'b', 'b', 'b', 'o',
+    'b', 'o', 'b', 'o', 'b',
+    'o', 'b', 'b', 'b'
+  ],
+  [
+    'b', 'b', 'b', 'b', 'b',
+    'b', 'b', 'b', 'b', 'b',
+    'o', 'o', 'b', 'o', 'b',
+    'o', 'b', 'b', 'b'
+  ],
+  [
+    'b', 'b', 'b', 'b', 'b',
+    'b', 'b', 'b', 'b', 'b',
+    'b', 'b', 'b', 'b', 'o',
+    'b', 'b', 'b', 'b'
+  ],
+  ['b', 'b', 'b', 'b', 'b',
+    'b', 'b', 'b', 'b', 'b',
+    'b', 'b', 'b', 'b', 'b',
+    'b', 'b', 'b', 'b'],
+  [
+    'o', 'o', 'b', 'b', 'b',
+    'b', 'b', 'b', 'b', 'b',
+    'b', 'b', 'b', 'b', 'b',
+    'b', 'b', 'b', 'b'
+  ],
+  [
+    'b', 'o', 'b', 'b', 'b',
+    'b', 'b', 'b', 'b', 'b',
+    'o', 'b', 'b', 'b', 'b',
+    'b', 'b', 'b', 'b'
+  ],
+  [
+    'b', 'o', 'b', 'o', 'b',
+    'b', 'b', 'b', 'b', 'o',
+    'o', 'b', 'b', 'b', 'b',
+    'b', 'b', 'b', 'b'
+  ],
+  [
+    'b', 'b', 'o', 'o', 'b',
+    'b', 'b', 'b', 'b', 'b',
+    'b', 'b', 'b', 'b', 'b',
+    'b', 'b', 'b', 'b'
+  ]
+]
+
+console.log(snarkArr.length);
+// const line = "8b2o$8bobo$10bo4b2o$6b4ob2o2bo2bo$6bo2bo3bobob2o$9bobobobo$10b2obobo$\n"
+// console.log(line.length);
