a96ec20d7c2b61a64be817a746cb6e4b415d2257
David Andrea <davejandrea@gmail.com>
2024-05-04 12:48:59 -0300

    add example for running app from command line.

--- src/main.ts
+++ src/main.ts
@@ -15,4 +15,17 @@ const start = async () => {
 
 start();
 
-// add arguments after 'npm start'
+// examples for manual testing/demonstration
+
+// Gosper Gun after 3 generations
+// npm start test/rle-files/gosper-gun.rle 3
+
+const result = "x = 36, y = 9, rule = B3/S23\n" +
+  "22bo$21bobo$11b2o6b2o3bo9b2o$10bobo4b2obo3bo9b2o$2o7b3o4b3obo3bo$2o6b\n" +
+  "3o4bo2b2obobo$9b3o4b2o4bo$10bobo$11b2o!\n"
+
+const expected = "x = 36, y = 9, rule = B3/S23\n" +
+"22bo$21bobo$11b2o6b2o3bo9b2o$10bobo4b2obo3bo9b2o$2o7b3o4b3obo3bo$2o6b\n" +
+"3o4bo2b2obobo$9b3o4b2o4bo$10bobo$11b2o!\n"
+
+console.log("Gosper Gun:", result === expected);
